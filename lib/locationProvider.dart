974e1b5c77477938a3daf29b9cd49fea137097ff
Anjan Roy <anjanroy@yandex.com>
2020-07-30 20:05:20 +0530

    seperated classes into different files

---
+++ lib/locationProvider.dart
@@ -0,0 +1,6 @@
+/// Location data provider identifier, either GPS based fine location data
+/// or Network based coarse location data, can be requested
+class LocationProvider {
+  static const String Network = 'network';
+  static const String GPS = 'gps';
+}
